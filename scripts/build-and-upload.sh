fddf4f21c88c2cca42cf072c256c226821f8e6e5
Andrew Gahov <andrew.gahov@gmail.com>
2023-10-15 15:09:00 +0200

    AT-2 Make a build script running via certain branch

--- scripts/build-and-upload.sh
+++ scripts/build-and-upload.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-# # # WARNING # # #
-# before the first running set this script as executable
+# WARNING: Before the first run, set this script as executable with
 # chmod +x ./scripts/build-and-upload.sh
 
-echo "Please enter your Docker Hub Access Token (for repository DIVO_IN_UA): "
-read -r -s DOCKER_HUB_ACCESS_TOKEN # The '-s' flag hides the input for security
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --branch)
+      branch="$2"
+      shift 2
+      ;;
+    -t|--token)
+      DOCKER_HUB_ACCESS_TOKEN="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$branch" ]; then
+  echo "Please provide the Git branch name using the --branch argument."
+  exit 1
+fi
+
+if [ -z "$DOCKER_HUB_ACCESS_TOKEN" ]; then
+  echo "Please provide the Docker Hub Access Token using the --token argument."
+  exit 1
+fi
 
 if docker login -u divoinua -p "$DOCKER_HUB_ACCESS_TOKEN"; then
   echo "Login successful!"
   timestamp=$(date +'%Y%m%d%H%M%S') # Set timestamp
+
+  # Fetch the latest updates for the specified Git branch
+  git fetch origin "$branch"
+
+  # Check out the desired Git branch
+  git checkout "$branch"
 
   # Build the JAR using Gradle, Build the Docker Container, Push it to the Docker Hub, logout
   ./gradlew build \
@@ -22,5 +51,4 @@ else
   echo "Login failed. Please check your password."
   exit 1  # Exit the script with an error status
 fi
-
 
