c11eeff11cea0f22b49fcf5f7d2e54077d46f34f
Andrew Gahov <andrew.gahov@gmail.com>
2023-10-19 19:34:15 +0200

    AT-2 change the main Controller from index to api

--- src/main/kotlin/divo/auto/controllers/ApiController.kt
+++ src/main/kotlin/divo/auto/controllers/ApiController.kt
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class IndexController {
-    @GetMapping("/")
+class ApiController {
+    @GetMapping("/api")
     fun index(): String {
         return "Hello, World"
     }
