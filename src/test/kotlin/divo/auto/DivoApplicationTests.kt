679ea12782b52de6951c86c5b2fab3949769c438
Andrew Gahov <andrew.gahov@gmail.com>
2023-11-05 23:00:00 +0100

    update test

--- src/test/kotlin/divo/auto/DivoApplicationTests.kt
+++ src/test/kotlin/divo/auto/DivoApplicationTests.kt
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 
 @SpringBootTest
+@ActiveProfiles("development")
 @AutoConfigureMockMvc
 class DivoApplicationTests {
 
